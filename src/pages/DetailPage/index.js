a4c12c87937bdbcd3fca1350fb422a8f57ff762d
seungyoonLee <syoon624@naver.com>
2022-12-28 21:56:56 +0900

    검색창 구현

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`/movie/${movieId}`);
+        console.log(response);
+        setMovie(response.data);
+      } catch (error) {
+        console.log(error);
+        navigate(-1);
+      }
+    }
+    fetchData();
+  }, [movieId]);
+  return (
+    <section>
+      <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="poster" />
+    </section>
+  );
 };
 
 export default DetailPage;
